367cd34484008b723ee2c9a2f0c135039c6efa87
EZGI COBAN <SARAHEZGICOBAN@GMAIL.COM>
2023-03-11 15:58:21 -0800

    Question and choices display

---
+++ server.js
@@ -0,0 +1,9 @@
+// const { Server } = require("socket.io");
+
+// const io = new Server({
+//   cors: {
+//     origin: "http://localhost:3000",
+//   },
+// });
+
+// io.listen(4000);
--- src/socket.js
+++ src/socket.js
@@ -4,5 +4,4 @@ import { io } from "socket.io-client";
 const URL =
   process.env.NODE_ENV === "production" ? undefined : "http://localhost:4000";
 
-export const doggo = io(URL);
-
+export const socket = io(URL);
